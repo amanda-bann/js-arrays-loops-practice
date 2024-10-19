ac15dd34c0ff1e2b20237184f7d648f4ad37df7d
amanda-bann <amanda_klap@protonmail.com>
2024-10-18 20:52:36 -0600

    Exercise #2 - Remove an element from an array

--- js/script.js
+++ js/script.js
@@ -11,8 +11,27 @@ var stuff = [
 ];
 
 // Exercise #1 - Log out even index items
-stuff.forEach(function (item, index) {
-  if(index % 2 === 0) {
-    console.log(item);
+//stuff.forEach(function (item, index) {
+//  if(index % 2 === 0) {
+//    console.log(item);
+// }
+//});
+
+// Exercise #2 - Remove an element from an array
+var removeItem = function (array, item) {
+  var index = array.indexOf(item);
+
+  if (index === -1) {
+    console.log(
+      `Sorry, no such item found in this array.`
+    );
+  } else {
+    array.splice(index, 1);
+    console.log(`Removed ${item} from the array.`);
   }
-});
+  };
+
+  removeItem(stuff, "coffee");
+  console.log(stuff);
+  removeItem(stuff, "sugar");
+  console.log(stuff);
